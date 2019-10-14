e8b997d59c292ad0fd8edf7c3e4de5e11b4d9bee
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2019-10-15 10:17:56 +1100

    Update test_shell_script.sh

--- test_shell_script.sh
+++ test_shell_script.sh
@@ -12,7 +12,6 @@ for nd in 100 200 300 400 500 1000 1500 2000 2500 3000 3500 4000
 do
     for i in {1..2}
     do
-        # python test_argparse.py --epochs $nd
-        echo epoch_${nd}_trial_${i}
+        python test_argparse.py --save_file exp_${nd}_trial_$i
     done
 done
