c10c78e6b893bb25cc6119586089f9e731f6ce81
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2019-10-15 10:15:15 +1100

    update shell script

--- run_2D_n_data_study.sh
+++ run_2D_n_data_study.sh
@@ -8,7 +8,7 @@
 # for nd in 100 200 300 400 500 1000 1500 2000 2500 3000 3500 4000
 for nd in 100 2000
 do
-    for i in {1..2}
+    for i in 1 2
     do
         python experiment_2D.py --n_data $nd --save_file n_data_study/exp_${nd}_trial_${i}
     done
