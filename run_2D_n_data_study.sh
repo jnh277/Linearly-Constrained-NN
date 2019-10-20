2f97f67919b805b82f4112d3ede419f706272765
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2019-10-21 09:39:15 +1100

    Update run_2D_n_data_study.sh

--- run_2D_n_data_study.sh
+++ run_2D_n_data_study.sh
@@ -9,8 +9,8 @@ for nd in 100 200 300 400 500 1000 1500 2000 2500 3000 3500 4000
 # for nd in 100
 do
     for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
-    for i in 1 2
+    for i in 1 2 3 4 5 6 7 8 9 10
     do
-        python experiment_2D.py --n_data $nd --save_file n_data_study3/exp_${nd}_trial_${i} --epochs 600
+        python experiment_2D.py --n_data $nd --save_file n_data_study/exp_${nd}_trial_${i} --epochs 400 --scheduler 1
     done
 done
