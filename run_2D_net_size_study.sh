60230897e518cee34d295a955586ed9c295fc4a4
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2019-10-22 12:40:26 +1100

    Update run_2D_net_size_study.sh

--- run_2D_net_size_study.sh
+++ run_2D_net_size_study.sh
@@ -7,14 +7,14 @@
 
 
 
-# for sc in 1 2 4 6 8 10 12 14 16 18 20
-for sc in 1 2
+for sc in 1 2 4 6 8 10 12 14 16 18 20
+# for sc in 1 2
 do
     for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
     # for i in 1 2
     do
         z=`expr $sc \* 10`
         v=`expr $sc \* 5`
-        python experiment_2D.py --n_data 2000 --net_hidden_size ${z} ${v} --save_file n_data_study/exp_${sc}_trial_${i} --epochs 400 --scheduler 1
+        python experiment_2D.py --n_data 4000 --net_hidden_size ${z} ${v} --save_file net_size_study/exp_${sc}_trial_${i} --epochs 400 --scheduler 1
     done
 done
