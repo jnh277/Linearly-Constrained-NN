abed5f96225033a93665d500d96636b4c42c23da
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2019-10-31 09:38:08 +1100

    Update run_mag_data_tests.sh

--- run_mag_data_tests.sh
+++ run_mag_data_tests.sh
@@ -13,7 +13,6 @@ do
     # for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
     # for i in 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
     do
-        python experiment_2D.py --n_data ${nd} --save_file n_data_study/exp_${nd}_trial_${i} --epochs 400 --scheduler 1
+        python mag_data_experiment.py --scheduler 1 --batch_size 250 --epochs 500 --n_train ${nd} --save_file mag_data_tests3/exp_${nd}_trial_${i} --net_hidden_size 150 75
     done
 done
-python mag_data_experiment.py --scheduler 1 --batch_size 250 --epochs 500 --n_train ${nd} --save_file mag_data_tests3/exp_${nd}_trial_${i} --net_hidden_size 150 75
