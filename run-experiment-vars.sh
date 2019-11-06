f299f56a6b97539513a2932ec1b7a79c9886b19f
Johannes <johannes.n.hendriks@gmail.com>
2019-11-06 13:41:56 +1100

    Update run-experiment-vars.sh

--- run-experiment-vars.sh
+++ run-experiment-vars.sh
@@ -11,6 +11,6 @@ cd /home/jnh277/Linearly-Constrained-NN
 
 echo "Running with ND=$ND and I=$I"
 
-python experiment_2D.py --n_data ${ND} --save_file n_data_study/exp_${ND}_trial_${I} --epochs 400 --scheduler 1 --display
+python experiment_2D.py --n_data ${ND} --save_file hcp/exp_${ND}_trial_${I} --epochs 400 --scheduler 1 --display
 
 
