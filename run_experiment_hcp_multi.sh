d513c04c1f05c4281fe51b486a80a530236a5d55
Johannes <johannes.n.hendriks@gmail.com>
2019-11-06 12:59:13 +1100

    Update run_experiment_hcp_multi.sh

--- run_experiment_hcp_multi.sh
+++ run_experiment_hcp_multi.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # make executable with the following
-# chmod u+x run_2D_n_data_study.sh
-# run using ./run_2D_n_data_study.sh
+# chmod u+x run_experiment_hcp_multi.sh
+# run using ./run_experiment_hcp_multi.sh
 
 
 for nd in 100 200 300
 do
     for i in 1 2 3
     do
-        qsub –v ND=$nd,I=$i run-experiment-vars.sh      # calls each job script
+        qsub –v ND=$nd,I=$i ./Linearly-Constrained-NN/run-experiment-vars.sh      # calls each job script
     done
 done
