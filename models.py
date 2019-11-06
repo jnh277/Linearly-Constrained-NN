9c729bf4c53c726db5ecd07e068eebe5bb534ecd
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2019-11-07 10:53:19 +1100

    Adds affine example

--- models.py
+++ models.py
@@ -82,6 +82,67 @@ class DerivNet2D(torch.nn.Module):
         v2 = -dydx1
         return (y, v1, v2)
 
+class AffineNet2D(torch.nn.Module):
+    def __init__(self, n_in, n_h1, n_h2, n_o):
+        super(AffineNet2D, self).__init__()
+        self.linear1 = torch.nn.Linear(n_in, n_h1)
+        self.tanh1 = torch.nn.Tanh()
+        self.linear2 = torch.nn.Linear(n_h1, n_h2)
+        self.tanh2 = torch.nn.Tanh()
+        self.linear3 = torch.nn.Linear(n_h2, n_o)
+        self.derivTanh1 = DerivTanh()
+        self.derivTanh2 = DerivTanh()
+        # self.affineLin = torch.nn.Linear(n_in, 2)
+        self.c = torch.nn.Parameter(torch.zeros(2, 1))
+        self.c = torch.nn.Parameter(torch.Tensor([[2.],[-0.5]]))
+        self.c.requires_grad = True
+        # self.c = torch.zeros(2,1)
+        # self.c[0] = 2.7
+        # self.c[1] = -0.9
+
+
+    def forward(self, x):
+        h1 = self.linear1(x)
+        z1 = self.tanh1(h1)
+        h2 = self.linear2(z1)
+        z2 = self.tanh2(h2)
+        y = self.linear3(z2)
+
+        # differential model
+        (nx, dx) = x.size()  # nx is number of data points, dx is data dimension (must match n_in)
+        w1 = self.linear1.weight
+        w2 = self.linear2.weight
+        w3 = self.linear3.weight
+
+        # derivative of h1 with respect to x1 (x-drection)
+        dh1dx1 = w1[:,0].unsqueeze(1).repeat(1, nx)
+
+        # derivative of h2 with respect to x2 (y-direction) # work out how to use expand instead
+        dh1dx2 = w1[:,1].unsqueeze(1).repeat(1, nx)
+
+        dh2dz1 = w2
+        dydz2 = w3
+
+        # print('size: ', dh2dz1.size())
+
+        dz1dh1 = self.derivTanh1(h1) # this shape means need to do some element wise multiplication
+        dz2dh2 = self.derivTanh2(h2)
+
+        # derivative of output with respect to x1
+        dydx1 = (dydz2.mm(dz2dh2 * dh2dz1.mm(dz1dh1 * dh1dx1))).t()
+
+        # derivative of output with respect to x2
+        dydx2 = (dydz2.mm(dz2dh2 * dh2dz1.mm(dz1dh1 * dh1dx2))).t()
+
+        # the affine part
+        # u = self.affineLin(x)
+        # v1 = dydx2 + u[:, 0].unsqueeze(1)
+        # v2 = -dydx1 + u[:, 1].unsqueeze(1)
+
+        v1 = dydx2 + self.c[0]*x[:, 0].unsqueeze(1)
+        v2 = -dydx1 + self.c[1]*x[:, 1].unsqueeze(1)
+        return (y, v1, v2)
+
 
 class DerivNet2D_v2(torch.nn.Module):
     def __init__(self, n_in, n_h1, n_h2, n_o):
