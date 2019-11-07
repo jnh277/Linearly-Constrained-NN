de268867e443f527be905aaafa4d889d093140e9
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2019-11-07 13:37:10 +1100

    delete unused data

--- run_experiment_hcp_multi.sh
+++ run_experiment_hcp_multi.sh
@@ -5,9 +5,9 @@
 # run using ./run_experiment_hcp_multi.sh
 
 
-for nd in 100 200 300
+for nd in 250 500 1000 3000 6000
 do
-    for i in 1 2 3
+    for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
     do
          # calls each job script
         qsub -v ND=${nd},I=${i} run-experiment-vars.sh
