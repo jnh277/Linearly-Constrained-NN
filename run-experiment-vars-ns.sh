211aac9c535ad05d33adc82a7c2a0cdb62b703ee
Johannes Hendriks <johannes.n.hendriks@gmail.com>
2019-11-07 15:28:32 +1100

    Adds job script

---
+++ run-experiment-vars-ns.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=1:mem=4GB
+#PBS -l walltime=10:00:00
+#PBS -l software=torch
+#PBS -k oe
+
+source /etc/profile.d/modules.sh
+module load torch/1.0.1-python3.6
+
+cd /home/jnh277/Linearly-Constrained-NN
+
+echo "Running with Z=$Z and V=$V and I=$I"
+
+python mag_data_experiment.py --scheduler 1 --batch_size 250 --epochs 500 --n_train ${ND} --save_file mag_data_n_study/exp_${ND}_trial_${I} --net_hidden_size ${Z} ${V}
+
---
+++ run_exp_hcp_netsize.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# make executable with the following
+# chmod u+x run_experiment_hcp_multi.sh
+# run using ./run_experiment_hcp_multi.sh
+
+
+for sc in 3 6 9 12 15 18 21
+do
+    for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
+    do
+        z=`expr $sc \* 10`
+        v=`expr $sc \* 5`
+         # calls each job script
+        qsub -v Z=${z},V=${v},I=${i} run-experiment-vars-ns.sh
+    done
+done
