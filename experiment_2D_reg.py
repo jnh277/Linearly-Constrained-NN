010f028b5ac8c262755da94804d87e9215d97db0
jnh277 <johannes.n.hendriks@gmail.com>
2020-04-17 13:15:20 +1000

    trying to get more trials running for conference review but hpc is fucked

--- experiment_2D_reg.py
+++ experiment_2D_reg.py
@@ -35,6 +35,8 @@ parser.add_argument('--pin_memory', action='store_true',
                     help='enables pin memory (default:False)')
 parser.add_argument('--scheduler', type=int, default=0,
                     help='0 selects interval reduction, 1 selects plateau (default:0)')
+parser.add_argument('--weight_decay', type=float, default=0,
+                    help='adds l2-norm regularisation')
 # for this problem using cuda was slower so have removed teh associated code
 # parser.add_argument('--cuda', action='store_true',
 #                     help='Enable cuda, will use cuda:0 (default:False)')
@@ -176,6 +178,66 @@ x_pred = torch.cat((xv.reshape(20 * 20, 1), yv.reshape(20 * 20, 1)), 1)
 error_new = torch.cat((v1.reshape(400, 1) - v1_pred.detach(), v2.reshape(400, 1) - v2_pred.detach()), 0)
 rms_error = torch.sqrt(sum(error_new * error_new) / 800)
 
+
+# ---------------  Set up and train the constrained model with regularisation -------------------------------
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=args.weight_decay)   # these should also be setable parameters
+if args.scheduler == 1:
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=10,
+                                                     min_lr=1e-10,
+                                                     factor=0.5,
+                                                    cooldown=15)
+else:
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 100, gamma=0.5, last_epoch=-1)
+# scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 25, gamma=0.5, last_epoch=-1)
+
+def train(epoch):
+    model.train()
+    total_loss = 0
+    n_batches = 0
+    for x1_train, x2_train, y1_train, y2_train in training_generator:
+        optimizer.zero_grad()
+        x_train = torch.cat((x1_train, x2_train), 1)
+        (yhat, v1hat, v2hat) = model(x_train)
+        loss = (criterion(y1_train, v1hat) + criterion(y2_train, v2hat)) / 2  # divide by 2 as it is a mean
+        loss.backward()
+        optimizer.step()
+        total_loss += loss
+        n_batches += 1
+    return total_loss / n_batches
+
+def eval(epoch):
+    model.eval()
+    # with torch.no_grad():
+    (yhat, v1hat, v2hat) = model(x_val)
+    loss = (criterion(y1_val, v1hat) + criterion(y2_val, v2hat)) / 2
+    return loss.cpu()
+
+
+train_loss_reg = np.empty([args.epochs, 1])
+val_loss_reg = np.empty([args.epochs, 1])
+
+if args.display:
+    print('Training invariant NN')
+
+for epoch in range(args.epochs):
+    train_loss_reg[epoch] = train(epoch).detach().numpy()
+    v_loss = eval(epoch)
+    if args.scheduler == 1:
+        scheduler.step(v_loss)
+    else:
+        scheduler.step(epoch)   # input epoch for scheduled lr, val_loss for plateau
+    val_loss_reg[epoch] = v_loss.detach().numpy()
+    if args.display:
+        print(args.save_file, 'Invariant NN: epoch: ', epoch, 'training loss ', train_loss[epoch], 'validation loss', val_loss[epoch])
+
+
+# work out the rms error for this one
+x_pred = torch.cat((xv.reshape(20 * 20, 1), yv.reshape(20 * 20, 1)), 1)
+(f_pred, v1_pred, v2_pred) = model(x_pred)
+error_new_reg = torch.cat((v1.reshape(400, 1) - v1_pred.detach(), v2.reshape(400, 1) - v2_pred.detach()), 0)
+rms_error_reg = torch.sqrt(sum(error_new_reg * error_new_reg) / 800)
+
 # ---------------  Set up and train the uncconstrained model -------------------------------
 optimizer_uc = torch.optim.Adam(model_uc.parameters(), lr=0.01)
 if args.scheduler == 1:
@@ -238,6 +300,69 @@ v2_pred_uc = v_pred_uc[:, 1]
 error_uc = torch.cat((v1.reshape(400) - v1_pred_uc.detach(), v2.reshape(400) - v2_pred_uc.detach()), 0)
 rms_uc = torch.sqrt(sum(error_uc * error_uc) / 800)
 
+
+# ---------------  Set up and train the uncconstrained model with weight decay -------------------------------
+optimizer_uc = torch.optim.Adam(model_uc.parameters(), lr=0.01, weight_decay=args.weight_decay)
+if args.scheduler == 1:
+    scheduler_uc = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer_uc, patience=10,
+                                                     min_lr=1e-10,
+                                                    factor=0.5,
+                                                    cooldown=15)
+else:
+    scheduler_uc = torch.optim.lr_scheduler.StepLR(optimizer_uc, 100, gamma=0.5, last_epoch=-1)
+
+def train_uc(epoch):
+    model_uc.train()
+    total_loss = 0
+    n_batches = 0
+    for x1_train, x2_train, y1_train, y2_train in training_generator:
+        optimizer_uc.zero_grad()
+        x_train = torch.cat((x1_train, x2_train), 1)
+        vhat = model_uc(x_train)
+        y_train = torch.cat((y1_train, y2_train), 1)
+        loss = criterion(y_train, vhat)
+        loss.backward()
+        optimizer_uc.step()
+        total_loss += loss.cpu()
+        n_batches += 1
+    return total_loss / n_batches
+
+def eval_uc(epoch):
+    model_uc.eval()
+    with torch.no_grad():
+        (vhat) = model_uc(x_val)
+        loss = criterion(y_val, vhat)
+    return loss.cpu()
+
+
+train_loss_uc_reg = np.empty([args.epochs, 1])
+val_loss_uc_reg = np.empty([args.epochs, 1])
+
+if args.display:
+    print('Training regularised standard NN')
+
+for epoch in range(args.epochs):
+    train_loss_uc_reg[epoch] = train_uc(epoch).detach().numpy()
+    v_loss = eval_uc(epoch)
+    if args.scheduler == 1:
+        scheduler_uc.step(v_loss)
+    else:
+        scheduler_uc.step(epoch)
+    val_loss_uc_reg[epoch] = v_loss.detach().numpy()
+    if args.display:
+        print(args.save_file, 'Reg Standard NN: epoch: ', epoch, 'training loss ', train_loss_uc[epoch], 'validation loss', val_loss_uc[epoch])
+
+# move model to cpu
+
+# work out final rms error for unconstrainted net
+# work out the rms error for this trial
+(v_pred_uc) = model_uc(x_pred)
+v1_pred_uc = v_pred_uc[:, 0]
+v2_pred_uc = v_pred_uc[:, 1]
+
+error_uc_reg = torch.cat((v1.reshape(400) - v1_pred_uc.detach(), v2.reshape(400) - v2_pred_uc.detach()), 0)
+rms_uc_reg = torch.sqrt(sum(error_uc_reg * error_uc_reg) / 800)
+
 # ----------------- save configuration options and results -------------------------------
 if args.save_file is not '':
     if args.display:
@@ -245,15 +370,23 @@ if args.save_file is not '':
     data = vars(args)       # puts the config options into a dict
     data['train_loss'] = train_loss
     data['val_loss'] = val_loss
+    data['train_loss_reg'] = train_loss_reg
+    data['val_loss_reg'] = val_loss_reg
     data['train_loss_uc'] = train_loss_uc
     data['val_loss_uc'] = val_loss_uc
     data['final_rms_error'] = rms_error.detach().numpy()        # these are tensors so have to convert to numpy
     data['final_rms_error_uc'] = rms_uc.detach().numpy()
+    data['train_loss_uc_reg'] = train_loss_uc_reg
+    data['val_loss_uc_reg'] = val_loss_uc_reg
+    data['final_rms_error_uc_reg'] = rms_uc_reg.detach().numpy()
+    data['final_rms_error_reg'] = rms_error_reg.detach().numpy()
     sio.savemat('./results/'+ args.save_file+'.mat', data)
 
 
 print(rms_error.detach().numpy())
+print(rms_error_reg.detach().numpy())
 print(rms_uc.detach().numpy())
+print(rms_uc_reg.detach().numpy())
 
 if args.display:
     print('Finished')
--- run-experiment-vars.sh
+++ run-experiment-vars.sh
@@ -11,5 +11,7 @@ cd /home/jnh277/Linearly-Constrained-NN
 
 echo "Running with ND=$ND and I=$I"
 
-python mag_data_experiment.py --scheduler 1 --batch_size 250 --epochs 500 --n_train ${ND} --save_file mag_data_n_study2/exp_${ND}_trial_${I} --net_hidden_size 150 75
+#python mag_data_experiment.py --scheduler 1 --batch_size 250 --epochs 500 --n_train ${ND} --save_file mag_data_n_study2/exp_${ND}_trial_${I} --net_hidden_size 150 75
 #python experiment_2D.py --n_data ${ND} --save_file n_data_study200/exp_${ND}_trial_${I} --epochs 400 --scheduler 1
+python experiment_2D.py --n_data ${ND} --save_file hcp/exp_${ND}_trial_${I} --epochs 400 --scheduler 1
+#python experiment_2D_reg.py --weight_decay 0.001 --n_data ${ND} --save_file n_data_study_reg1/exp_${ND}_trial_${I} --epochs 400 --scheduler 1
