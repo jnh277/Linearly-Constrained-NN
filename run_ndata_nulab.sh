126c8e8e02ea1b8a1fddcf66e1c91122be1c6f41
Tim Linux <tkfarnworth@gmail.com>
2020-04-20 11:20:34 +1000

    Adds simulation results with regularisation

--- run_ndata_nulab.sh
+++ run_ndata_nulab.sh
@@ -1,13 +1,19 @@
-# chmod u+x run_experiment_hcp_multi.sh
-# run using ./run_experiment_hcp_multi.sh
+#!/bin/bash
+echo "Bash version ${BASH_VERSION}..."
+
+# chmod u+x run_ndata_nulab.sh
+# run using ./run_ndata_nulab.sh
 
 #for nd in 100 200 300 400 500 1000 1500 2000 2500 3000 3500 4000
 for nd in 100 200 500 1000 2000 3000
+#for nd in 100 200
 do
   for i in {1..25}
+  # for i in {1..2}
     do
         echo "Running with nd=$nd and i=$i"
         # runs python
-       python experiment_2D_reg.py --weight_decay 0.001 --n_data ${nd} --save_file n_data_study_reg1/exp_${nd}_trial_${i} --epochs 400 --scheduler 1
+       python3 experiment_2D_reg.py --weight_decay 0.005 --n_data ${nd} --save_file n_data_study_reg005/exp_${nd}_trial_${i} --epochs 400 --scheduler 1
     done
 done
+
