86a2cb24723c5113d4306cb8ab28bb8ebfdc2e84
jnh277 <johannes.n.hendriks@gmail.com>
2020-04-28 15:46:33 +1000

    Getting ready for a hopefully succesful run on the HPC

--- run-experiment-vars.sh
+++ run-experiment-vars.sh
@@ -14,5 +14,5 @@ echo "Running with ND=$ND and I=$I"
 
 #python mag_data_experiment.py --scheduler 1 --batch_size 250 --epochs 500 --n_train ${ND} --save_file mag_data_n_study2/exp_${ND}_trial_${I} --net_hidden_size 150 75
 #python experiment_2D.py --n_data ${ND} --save_file n_data_study200/exp_${ND}_trial_${I} --epochs 400 --scheduler 1
-python experiment_2D.py --n_data ${ND} --save_file hcp/exp_${ND}_trial_${I} --epochs 400 --scheduler 1 --display
-#python experiment_2D_reg.py --weight_decay 0.01 --n_data ${ND} --save_file n_data_study_reg01/exp_${ND}_trial_${I} --epochs 20 --scheduler 1
+#python experiment_2D.py --n_data ${ND} --save_file hcp/exp_${ND}_trial_${I} --epochs 400 --scheduler 1 --display
+python experiment_2D_reg.py --weight_decay 0.00075 --n_data ${ND} --save_file n_data_study_reg00075/exp_${ND}_trial_${I} --epochs 400 --scheduler 1 --display
