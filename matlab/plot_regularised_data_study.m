0d16b7a698b593b467e17b98dca4c5fb49e8f0d6
jnh277 <johannes.n.hendriks@gmail.com>
2020-05-01 16:03:00 +1000

    Update plot_regularised_data_study.m

--- matlab/plot_regularised_data_study.m
+++ matlab/plot_regularised_data_study.m
@@ -2,11 +2,11 @@
 clear all
 clc
 
-% files = dir('../results/mag_data_n_study/*.mat');
-% files = dir('../results/n_data_study_reg001/*.mat');
-% files = dir('../results/n_data_study_reg00075/*.mat');
-files = dir('../results/n_data_study_reg0005/*.mat');
-files = dir('../results/n_data_study_reg0002/*.mat');
+% files = dir('../results/mag_data_n_study/*.mat');         % bug
+% files = dir('../results/n_data_study_reg001/*.mat');      % bug
+% files = dir('../results/n_data_study_reg00075/*.mat');    % bug
+% files = dir('../results/n_data_study_reg0005/*.mat');     % bug
+% files = dir('../results/n_data_study_reg0002/*.mat');     % bug
 
 for i = 1:length(files)
     r(i) = load(strcat(files(i).folder,'/',files(i).name));
